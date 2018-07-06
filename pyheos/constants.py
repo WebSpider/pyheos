07d8a1097a8910563f9e452ea311fe0bbf69ef64
WebSpider <webspider@users.noreply.github.com>
2018-07-06 03:02:48 +0200

    WIP: Adding commands registry, setting up ssdp

--- pyheos/constants.py
+++ pyheos/constants.py
@@ -29,3 +29,60 @@ SOURCE_HEOS_FAVORITES = 1028
 
 # HEOS Defaults
 HEOS_PORT = 1255
+HEOS_ST_NAME = 'urn:schemas-denon-com:device:ACT-Denon:1'
+
+# UPNP Defaults
+SSDP_HOST = '239.255.255.250'
+SSDP_PORT = 1900
+
+# Credentials
+HEOS_USERNAME = 'dummy'
+HEOS_PASSWORD = 'denon'
+
+# Commands
+COMMAND_REGISTRY = {
+    'CMD_REGISTER_CHANGE_EVENTS': 'system/register_for_change_events',
+    'CMD_HEOS_ACCOUNT_CHECK': 'system/check_account',
+    'CMD_HEOS_ACCOUNT_SIGNIN': 'system/sign_in',
+    'CMD_HEOS_ACCOUNT_SIGNOUT': 'system/sign_out',
+    'CMD_HEOS_HEARTBEAT': 'system/heart_beat',
+    'CMD_HEOS_REBOOT': 'system/reboot',
+    'CMD_PRETTIFY_JSON': 'system/prettfy_json_response',
+    'CMD_GET_PLAYERS': 'player/get_players',
+    'CMD_GET_PLAYER_INFO': 'player/get_player_info',
+    'CMD_GET_PLAY_STATE': 'player/get_play_state',
+    'CMD_SET_PLAY_STATE': 'player/set_play_state',
+    'CMD_GET_NOW_PLAYING': 'player/get_now_playing_media',
+    'CMD_GET_VOLUME': 'player/get_volume',
+    'CMD_SET_VOLUME': 'player/set_volume',
+    'CMD_VOLUME_UP': 'player/volume_up',
+    'CMD_VOLUME_DOWN': 'player/volume_down',
+    'CMD_GET_MUTE': 'player/get_mute',
+    'CMD_SET_MUTE': 'player/set_mute',
+    'CMD_TOGGLE_MUTE': 'player/toggle_mute',
+    'CMD_GET_PLAY_MODE': 'player/get_play_mode',
+    'CMD_SET_PLAY_MODE': 'player/set_play_mode',
+    'CMD_GET_QUEUE': 'player/get_queue',
+    'CMD_PLAY_QUEUE_ITEM': 'player/play_queue',
+    'CMD_REMOVE_FROM_QUEUE': 'player/remove_from_queue',
+    'CMD_SAVE_QUEUE_AS_PLAYLIST': 'player/save_queue',
+    'CMD_CLEAR_QUEUE': 'player/clear_queue',
+    'CMD_MOVE_TO_QUEUE': 'player/move_queue_item',
+    'CMD_PLAY_NEXT': 'player/play_next',
+    'CMD_PLAY_PREV': 'player/play_previous',
+    'CMD_SET_QUICKSELECT': 'player/set_quickselect',
+    'CMD_PLAY_QUICKSELECT': 'player/play_quickselect',
+    'CMD_GET_QUICKSELECTS': 'player/get_quickselects',
+    'CMD_CHECK_FW_UPDATE': 'player/check_update',
+    'CMD_GET_GROUPS': 'player/get_groups',
+    'CMD_GET_GROUP_INFO': 'group/get_group_info',
+    'CMD_SET_GROUP': 'group/set_group',
+    'CMD_GET_GROUP_VOLUME': 'group/get_volume',
+    'CMD_SET_GROUP_VOLUME': 'group/set_volume',
+    'CMD_GROUP_VOLUME_UP': 'group/volume_up',
+    'CMD_GROUP_VOLUME_DOWN': 'group/volume_down',
+    'CMD_GET_GROUP_MUTE': 'group/get_mute',
+    'CMD_SET_GROUP_MUTE': 'group/set_mute',
+    'CMD_TOGGLE_GROUP_MUTE': 'group/toggle_mute',
+    'CMD_'
+}
--- pyheos/heos.py
+++ pyheos/heos.py
@@ -4,6 +4,7 @@ Python bindings for Denon HEOS
 """
 
 import asyncio
+from .constants import HEOS_PORT, HEOS_ST_NAME, HEOS_USERNAME, HEOS_PASSWORD, COMMAND_REGISTRY
 
 class HEOSException(Exception):
     """ Generic HEOS Exception class """
@@ -16,12 +17,14 @@ class HEOSException(Exception):
 class HEOS(object):
     """ Generic class for a HEOS system """
 
-    def __init__(self):
-        self._host = None
-        self._port = None
+    def __init__(self, loop, host=None, port=None):
+        self._host = host
+        self._port = port
+        self._loop = loop
+
 
     @asyncio.coroutine
-    async def connect(self, host=None, port=None, callback=None):
+    async def connect(self, host=None, port=HEOS_PORT, callback=None):
         """ Connect to HEOS """
 
         if host is not None:
@@ -30,7 +33,22 @@ class HEOS(object):
         if port is not None:
             self._port = port
 
-        yield from self._connect(self, host, port)
+        # Perform discovery if needed
+        if not self._host:
+            from .ssdp import discover
+            url = _filter_ssdp_response(responses=discover(), st=HEOS_ST_NAME)
+            self._host = self._url_converter(url)
+
+        yield from self._connect(self._host, port)
+
+        # We are now connected. Let's initialize.
+        # CLI 2.1.1
+        self.unregister_for_change_events()
+        self.sign_in(username=HEOS_USERNAME, password=HEOS_PASSWORD)
+        self.get_heos_state()
+        self.register_for_change_events()
+        if self._subscription_task is None:
+            self._subscription_task = self._loop.create_task(self._async_subscribe(callback))
 
     @asyncio.coroutine
     async def _connect(self, host, port):
@@ -42,4 +60,29 @@ class HEOS(object):
             except (TimeoutError, ConnectionRefusedError):
                 return
 
+    @staticmethod
+    def _url_converter(self, url):
+        import re
 
+        pieces = re.search('(https?://)([^:^/]*)[:\\d*]?(.*)?$', url)
+
+        if pieces:
+            return pieces.group(2)
+        else:
+            return None
+
+    @staticmethod
+    def _filter_ssdp_response(self, responses, st):
+        for response in responses:
+            if response.st == st
+                return response.location
+
+    def send_command(self, command, payload):
+        """ Sends a command to a known destination """
+        message = 'heos://' + str(command)
+        if payload:
+            message += '?' + '&'.join("{}={}".format(k, v) for (k, v) in payload.items())
+
+        self._writer.write(msg.encode('ascii'))
+
+    def unregister_for_change_events(self):
